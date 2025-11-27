fa1929685637b467b9ec024de1bc06c52b78836d
you <you@example.com>
2025-11-27 10:44:11 +0200

    Table rename

--- database/migrations/2025_11_27_083659_create_tag_product_table.php
+++ database/migrations/2025_11_27_083659_create_tag_product_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tag_product', function (Blueprint $table) {
+        Schema::create('product_tag', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')->contrained()->onDelete('cascade');
             $table->foreignId('tag_id')->contrained()->onDelete('cascade');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tag_product');
+        Schema::dropIfExists('product_tag');
     }
 };
